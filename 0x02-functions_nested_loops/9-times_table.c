caeec05605f58a717b749132b3535c81d1e105ac
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-02-23 12:23:30 +0200

    Create  9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if (j != 0)
+				_putchar(',');
+			if (r >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + r / 10);
+				_putchar('0' + r % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + r);
+			}
+		}
+		_putchar('\n');
+	}
+}
