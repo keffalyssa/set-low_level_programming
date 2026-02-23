cccb8b6512fd6636702ef4c94c6b51dc2469249f
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-02-23 12:19:36 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @i: integer
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	return (i);
+}
