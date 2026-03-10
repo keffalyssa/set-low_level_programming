8668d01ac62394300b4c65ef92830e7b62b6051d
keffalyssa <keffalyssak@gmail.com>
2026-03-10 05:41:35 -0700

    Fix 1-main.c comment out putchar

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,14 +1,24 @@
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-	/* Your code here */
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		/*putchar(i);*/
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
-	
-
-
-       
