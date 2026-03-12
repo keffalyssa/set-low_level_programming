60a5b933739e5c07c8ce02f1d8c49d5b955fc4af
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-03-12 11:24:10 +0200

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,20 @@
+cd ~/0x04-more_functions_nested_loops
+cat > 1-isdigit.c << 'EOF'
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit
- * @c: character to check
- * Return: 1 if digit, 0 otherwise
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: the character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
-return (1);
-return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
+EOF
+git add 1-isdigit.c
+git commit -m "Fix Betty style in _isdigit"
+git push -u origin master
