e407f6b2cb79e279967cf10f423925848a8618c7
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-03-12 15:19:33 +0200

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search
+ * @accept: the accepted characters
+ *
+ * Return: number of bytes in initial segment of s from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	int i;
+	int found;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		found = 0;
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (s[count] == accept[i])
+			{
+				found = 1;
+				break;
+			}
+			i++;
+		}
+		if (found == 0)
+			break;
+		count++;
+	}
+	return (count);
+}
