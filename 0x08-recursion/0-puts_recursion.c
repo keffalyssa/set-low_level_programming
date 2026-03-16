997ba4149764baa33a5d147e0422838272b01b8c
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-03-16 12:51:52 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,6 +3,8 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: the string to print
+ *
+ * Return: Nothing
  */
 void _puts_recursion(char *s)
 {
@@ -14,36 +16,3 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-```
-
----
-
-**File 2: `README.md`**
-```
-# 0x08. C - Recursion
-
-## Description
-This project covers the concept of recursion in C programming.
-Recursion is a technique where a function calls itself to solve
-smaller instances of a problem until it reaches a base case.
-
-## Learning Objectives
-- What is recursion
-- How to implement recursion
-- When to implement recursion
-- When not to implement recursion
-
-## Requirements
-- Allowed editors: vi, vim, emacs
-- Compiled on Ubuntu 20.04 LTS using gcc
-- Flags: -Wall -Werror -Wextra -pedantic -std=gnu89
-- No global variables allowed
-- No more than 5 functions per file
-- No static variables allowed
-
-## Files
-- 0-puts_recursion.c - Prints a string followed by a new line
-
-## Repository
-- GitHub repository: set-low_level_programming
-- Directory: 0x08-recursion
