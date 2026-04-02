41af24c336342475a26b57dde5e99e2b02e8d64b
Your Name <you@example.com>
2026-04-02 01:46:01 -0700

    Fix strstr and add libmy.a

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,26 +1,17 @@
-char *_strstr(char *h, char *n)
-{ int i; if(!*n)return(h); while(*h){i=0; while(n[i]&&h[i]==n[i])i++; if(!n[i])return(h); h++;} return(0); }
-EOFcat > main.h << 'EOF'
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-#endif
+char *_strstr(char *haystack, char *needle)
+{
+    int i;
+
+    if (!*needle)
+        return (haystack);
+    while (*haystack)
+    {
+        i = 0;
+        while (needle[i] && haystack[i] == needle[i])
+            i++;
+        if (!needle[i])
+            return (haystack);
+        haystack++;
+    }
+    return (0);
+}
