98ed9d6bfa6602d09dc322e787bb53053c0dba43
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-09 15:22:18 +0200

    Update 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
 
----
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ * If malloc fails, exit with status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
 
-## 🧠 Objective
+	ptr = malloc(b);
 
-Implement a function that safely allocates memory and handles failure properly.
+	if (ptr == NULL)
+		exit(98);
 
----
-
-## ⚙️ Function Description
-
-### malloc_checked
-
-```c
-void *malloc_checked(unsigned int b);
+	return (ptr);
+}
