fe68e6fdf4b35175886e82c593abe2f60b02f314
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-09 15:20:55 +0200

    Add malloc_checked function for safe memory allocation
    Implement malloc_checked function to allocate memory safely.

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+
+---
+
+## 🧠 Objective
+
+Implement a function that safely allocates memory and handles failure properly.
+
+---
+
+## ⚙️ Function Description
+
+### malloc_checked
+
+```c
+void *malloc_checked(unsigned int b);
