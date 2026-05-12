b0ae738fcb1808a8a56c1e2e5f661cf574021b5b
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-12 11:11:10 +0200

    Update 5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
+	*head = NULL;
+}
 	*head = NULL;
 }
