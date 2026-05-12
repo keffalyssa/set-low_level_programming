2b7e9b68b4caa9c6bec3ae9eabccc58f80347ed9
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-12 11:18:20 +0200

    Update lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -24,6 +24,4 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 
-#endif /* LISTS_H */d, const int n);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
+#endif /* LISTS_H */
