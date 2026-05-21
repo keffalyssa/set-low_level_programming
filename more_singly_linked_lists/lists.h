487c33089f36721c0124edfdc7452bb75f5d119d
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-21 10:52:23 +0200

    Update lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,1 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 int sum_listint(listint_t *head);
+
+#endif
