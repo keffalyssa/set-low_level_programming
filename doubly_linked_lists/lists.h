79f8597a689a60c31476f3b7d1f5de09dfc44d74
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-05-28 11:08:27 +0200

    Add delete_dnodeint_at_index function prototype

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -26,3 +26,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif /* LISTS_H */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
