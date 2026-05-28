60355ed1ce5ae299e5a5356325733c5eb35ce81c
keffalyssa <keffalyssa@github.com>
2026-05-28 09:37:59 +0000

    Fix 103-keygen split functions under 40 lines

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,56 +3,54 @@
 #include <string.h>
 
 /**
- * get_key - computes the key for a username
- * @username: the username string
- * @len: length of username
- *
- * Return: nothing, prints key
+ * compute_k0_k2 - computes first 3 key indices
+ * @u: username
+ * @len: length
+ * @k: output array
  */
-void get_key(char *username, int len)
+void compute_k0_k2(char *u, int len, int *k)
 {
-	char *cs = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	int k0, k1, k2, k3, k4, k5;
-	int sum, prod, max_c, sq_sum, i;
-	char key[7];
+	int i, sum, prod;
 
-	k0 = (len ^ 0x3b) & 0x3f;
+	k[0] = (len ^ 0x3b) & 0x3f;
 
 	sum = 0;
 	for (i = 0; i < len; i++)
-		sum += (unsigned char)username[i];
-	k1 = (sum ^ 0x4f) & 0x3f;
+		sum += (unsigned char)u[i];
+	k[1] = (sum ^ 0x4f) & 0x3f;
 
 	prod = 1;
 	for (i = 0; i < len; i++)
-		prod *= (unsigned char)username[i];
-	k2 = (prod ^ 0x55) & 0x3f;
+		prod *= (unsigned char)u[i];
+	k[2] = (prod ^ 0x55) & 0x3f;
+}
 
-	max_c = (unsigned char)username[0];
+/**
+ * compute_k3_k5 - computes last 3 key indices
+ * @u: username
+ * @len: length
+ * @k: output array
+ */
+void compute_k3_k5(char *u, int len, int *k)
+{
+	int i, max_c, sq_sum, val;
+
+	max_c = (unsigned char)u[0];
 	for (i = 1; i < len; i++)
-		if ((unsigned char)username[i] > max_c)
-			max_c = (unsigned char)username[i];
+		if ((unsigned char)u[i] > max_c)
+			max_c = (unsigned char)u[i];
 	srand(max_c ^ 0x0e);
-	k3 = rand() & 0x3f;
+	k[3] = rand() & 0x3f;
 
 	sq_sum = 0;
 	for (i = 0; i < len; i++)
-		sq_sum += (unsigned char)username[i] * (unsigned char)username[i];
-	k4 = (sq_sum ^ 0xef) & 0x3f;
+		sq_sum += (unsigned char)u[i] * (unsigned char)u[i];
+	k[4] = (sq_sum ^ 0xef) & 0x3f;
 
-	sum = 0;
-	for (i = 0; i < (unsigned char)username[0]; i++)
-		sum = rand();
-	k5 = (sum ^ 0xe5) & 0x3f;
-
-	key[0] = cs[k0];
-	key[1] = cs[k1];
-	key[2] = cs[k2];
-	key[3] = cs[k3];
-	key[4] = cs[k4];
-	key[5] = cs[k5];
-	key[6] = '\0';
-	printf("%s\n", key);
+	val = 0;
+	for (i = 0; i < (unsigned char)u[0]; i++)
+		val = rand();
+	k[5] = (val ^ 0xe5) & 0x3f;
 }
 
 /**
@@ -64,11 +62,21 @@ void get_key(char *username, int len)
  */
 int main(int argc, char *argv[])
 {
+	char *cs = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	int k[6], i, len;
+	char key[7];
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s username\n", argv[0]);
 		return (1);
 	}
-	get_key(argv[1], strlen(argv[1]));
+	len = strlen(argv[1]);
+	compute_k0_k2(argv[1], len, k);
+	compute_k3_k5(argv[1], len, k);
+	for (i = 0; i < 6; i++)
+		key[i] = cs[k[i]];
+	key[6] = '\0';
+	printf("%s\n", key);
 	return (0);
 }
