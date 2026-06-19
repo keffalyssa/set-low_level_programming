f45845cb0e4feaf60e2c20f3cea12cfc9bddc715
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-06-19 11:13:08 +0200

    Refactor hash_table_set to include update_value function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,25 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table
+ * update_value - Updates value of existing key
  * @ht: The hash table
- * @key: The key (cannot be empty string)
- * @value: The value associated with the key
+ * @key: The key to search
+ * @value: The new value
+ * @index: The index in the array
  *
- * Return: 1 if succeeded, 0 otherwise
+ * Return: 1 if updated, 0 if not found
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+static int update_value(hash_table_t *ht, const char *key,
+			const char *value, unsigned long int index)
 {
-	hash_node_t *new_node, *tmp;
-	unsigned long int index;
+	hash_node_t *tmp;
 	char *new_value;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
-
 	while (tmp)
 	{
 		if (strcmp(tmp->key, key) == 0)
@@ -33,7 +29,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
+	return (0);
+}
 
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key (cannot be empty string)
+ * @value: The value associated with the key
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (update_value(ht, key, value, index))
+		return (1);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
