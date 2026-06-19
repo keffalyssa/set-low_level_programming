92a97f139fe4f7016ab4ea1862e80bd288f8ecaa
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-06-19 10:17:18 +0200

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -33,3 +33,4 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
