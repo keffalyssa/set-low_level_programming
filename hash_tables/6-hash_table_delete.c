f83cc401ab6ffa4c58ca585984e1acaee3c6bde2
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-06-19 11:21:51 +0200

    Create 6-hash_table_delete.c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *next;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
