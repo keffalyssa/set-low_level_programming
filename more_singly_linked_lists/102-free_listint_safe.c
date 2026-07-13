ccb7ea1f2b3676e91608d483cee8e2859594d4c0
keffalyssa <keffalyssak@gmail.com>
2026-07-13 06:09:47 -0700

    Add task 5 free_listint_safe function and prototype

---
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (can free lists with a loop)
+ * @h: A pointer to the address of the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *curr, *next_node;
+size_t len = 0;
+long int diff;
+
+if (h == NULL || *h == NULL)
+return (0);
+
+curr = *h;
+while (curr != NULL)
+{
+diff = curr - curr->next;
+if (diff > 0)
+{
+next_node = curr->next;
+free(curr);
+curr = next_node;
+len++;
+}
+else
+{
+free(curr);
+curr = NULL;
+len++;
+break;
+}
+}
+
+*h = NULL;
+return (len);
+}
